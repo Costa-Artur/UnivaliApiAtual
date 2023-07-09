a1241475a3e472ddaae45be4a7bbab0e45b37279
Artur Costa <souza.artur@edu.univali.br>
2023-07-08 22:15:56 -0300

    configs

--- src/Univali.Api/Program.cs
+++ src/Univali.Api/Program.cs
@@ -89,13 +89,13 @@ builder.AddFluentValidationServices();
 builder.Services.AddDbContext<CustomerContext>(options => 
 {
     options
-    .UseNpgsql("Host=localhost;Database=Univali;Username=postgres;Password=Quintalmagico12#");
+    .UseNpgsql("Host=localhost;Database=Univali;Username=postgres;Password=123456");
 });
 
 builder.Services.AddDbContext<PublisherContext>(options => 
 {
     options
-    .UseNpgsql("Host=localhost;Database=Univali;Username=postgres;Password=Quintalmagico12#");
+    .UseNpgsql("Host=localhost;Database=Univali;Username=postgres;Password=123456");
 });
 
 builder.Services.AddControllers(options =>{
